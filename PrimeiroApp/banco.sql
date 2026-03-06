a7b4e8eee65031ad206fd3f5290d719f085ee03c
MaNogali <maribarosanogali@gmail.com>
2026-03-06 10:40:48 -0300

    Adicionando banco

---
+++ PrimeiroApp/banco.sql
@@ -0,0 +1,14 @@
+create database dbBancoApp;
+use dbBancoApp;
+create table usuario
+(
+IdUsu int primary KEY auto_increment,
+nomeUsu varchar(50) not null,
+Cargo varchar(50) not null,
+DataNasc datetime
+);
+insert into usuario(nomeUsu, Cargo, DataNasc)
+			values('Nilson', 'Gerente', '1978/05/01'),
+				('Bruno', 'Colaborador', '2000/10/12');
+select*from usuario;                
+                
